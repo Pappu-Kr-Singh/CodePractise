34780ef05b78df9ed88d98171aa51746bc45a3d4
Pappu Singh <pappu.singh159687@gmail.com>
2024-06-16 23:55:02 +0530

    added basic layout of social media web app

--- Projects/social-sphere/src/App.jsx
+++ Projects/social-sphere/src/App.jsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import SideBar from "./components/SideBar";
 
 function App() {
-  return <>Hello from social sphere</>;
+  return (
+    <>
+      <div className="app-container">
+        <SideBar />
+        <div className="content">
+          <Header />
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
